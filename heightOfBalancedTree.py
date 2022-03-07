8b1322410a3bb9c79474565bf65b99305f63f812
Viktor <vmac@itu.dk>
2022-03-07 20:22:05 +0100

    Adding algorithm determining whether the tree is height balanced

---
+++ heightOfBalancedTree.py
@@ -0,0 +1,40 @@
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+		
+class TreeInfo:
+	def __init__(self, isBalanced, height):
+		self.isBalanced = isBalanced
+		self.height = height
+
+
+def heightBalancedBinaryTree(tree):
+    # Write your code here.
+	treeInfo = checkTree(tree)
+	
+	if treeInfo.isBalanced:
+		return True
+	
+	return False
+
+def checkTree(tree):
+	if tree is None:
+		return TreeInfo(True, 0)
+	
+	leftTree = checkTree(tree.left)
+	rightTree = checkTree(tree.right)
+	
+	
+	diff = abs(leftTree.height - rightTree.height)
+	
+	if leftTree.isBalanced and rightTree.isBalanced and diff < 2:
+		return TreeInfo(True, max(leftTree.height, rightTree.height)+1)
+	else:
+		return TreeInfo(False, 0)
+	
+## COMPLEXITY:
+# time: O(n) where n is the number of nodes in the tree
+# space: O(h) where h is the height of the tree
