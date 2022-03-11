23f3cd87a16d0ef557a0c9e80b1829d895661fff
Viktor <vmac@itu.dk>
2022-03-11 22:59:48 +0100

    Finishing the kth node removal from linked list

--- kthNodeRemoval.py
+++ kthNodeRemoval.py
@@ -7,12 +7,31 @@ class LinkedList:
 
 def removeKthNodeFromEnd(head, k):
     # Write your code here.
-	currNode = head.next
+	currNode = head
 	
 	length = 1
 	
 	while currNode.next is not None:
+		length += 1
 		currNode = currNode.next
-		
+	
+	nodeBefore = head
+	
+	targetLen = length - k
 	currLen = 1
 	
+	if length == k:
+		head.value = head.next.value
+		head.next = head.next.next
+		return
+	
+	while currLen < targetLen and nodeBefore.next is not None:
+		nodeBefore = nodeBefore.next
+		currLen += 1
+		
+	nodeBefore.next = nodeBefore.next.next
+	
+## COMPLEIXITY:
+# time: O(n) where n is the number of elements in the linked list
+# space: O(1)
+
