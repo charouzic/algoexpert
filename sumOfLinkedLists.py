a954f228fdcf9e44e0e9660e0a531d9e9eb9b206
Viktor <vmac@itu.dk>
2022-03-12 13:09:36 +0100

    Adding the working solution

--- sumOfLinkedLists.py
+++ sumOfLinkedLists.py
@@ -19,12 +19,12 @@ def sumOfLinkedLists(linkedListOne, linkedListTwo):
 	reversedSuma = ''.join(reversed(res))
 	
 	
-	head = LinkedList(reversedSuma[0])
+	head = LinkedList(int(reversedSuma[0]))
 	
 	node = head
 	
 	for i in range(1, len(reversedSuma)):
-		node.next = LinkedList(reversedSuma[i])
+		node.next = LinkedList(int(reversedSuma[i]))
 		node = node.next
 		
 	return head
@@ -36,6 +36,19 @@ def getNumber(linkedList):
 	while node.next is not None:
 		number.append(str(node.value))   
 		node = node.next
+	# adding the last node value 
+	number.append(str(node.value))
 	# number = "2471"
 	reversedNumber = reversed(number)
 	return int(''.join(reversedNumber))
+
+firstHead = LinkedList(2)
+firstHead.next = LinkedList(4)
+firstHead.next.next = LinkedList(7)
+firstHead.next.next.next = LinkedList(1)
+
+secondHead = LinkedList(9)
+secondHead.next = LinkedList(4)
+secondHead.next.next = LinkedList(5)
+
+sumOfLinkedLists(firstHead, secondHead)
