f6720f543de643b63bbdd6d0b19ec637154ee1f7
Viktor <vmac@itu.dk>
2022-03-13 16:44:18 +0100

    Adding the working solution for mnemonics algorithm

--- phoneNumberMnemonics.py
+++ phoneNumberMnemonics.py
@@ -1,29 +1,37 @@
 def phoneNumberMnemonics(phoneNumber):
     # Write your code here.
-	letters = {
-		0: ["0"],
-		1: ["1"],
-		2: ["a","b","c"],
-		3: ["d","e","f"],
-		4: ["g","h","i"],
-		5: ["j","k","l"],
-		6: ["m","n","o"],
-		7: ["p","q","r","s"],
-		8: ["t","u","v"],
-		9: ["w","x","y","z"]
+	
+	mnemonic = ["0" for _ in range(len(phoneNumber))]
+	
+	return pickCharacter(0, phoneNumber, mnemonic, [])
+	
+
+def pickCharacter(idx,phoneNumber, currentMnemonic, res):
+	if idx == len(phoneNumber):
+		return res.append(''.join(currentMnemonic))
+	else:
+		listOfLetters = LETTERS[phoneNumber[idx]]
+		
+		for letter in listOfLetters:
+			currentMnemonic[idx] = letter
+			pickCharacter(idx + 1, phoneNumber, currentMnemonic, res)
+			
+	return res
+
+LETTERS = {
+		"0": ["0"],
+		"1": ["1"],
+		"2": ["a","b","c"],
+		"3": ["d","e","f"],
+		"4": ["g","h","i"],
+		"5": ["j","k","l"],
+		"6": ["m","n","o"],
+		"7": ["p","q","r","s"],
+		"8": ["t","u","v"],
+		"9": ["w","x","y","z"],
 		
 	}
 	
-	words = letters[phoneNumber[0]]
-	
-	for i in range(1, len(phoneNumber)):
-		
-		letters = letters[phoneNumber[i]]
-		
-		for j in range(len(words)):
-			word = words.pop(0)
-			for letter in letters:
-				words.append(word+letter)
-				
-	return words
-
+## COMPLEXITY:
+# time -> O(n*4^n) where n is the number of digits in the phone number
+# space -> O(n*4^n)
