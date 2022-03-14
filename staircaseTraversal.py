716947b73785052d2849a5f9fe7859faf6f936f7
Viktor <vmac@itu.dk>
2022-03-14 20:22:46 +0100

    Working staircase traversal algorithm

--- staircaseTraversal.py
+++ staircaseTraversal.py
@@ -5,17 +5,18 @@ def staircaseTraversal(height, maxSteps):
 # at each stair I will iterate through the numbers ranging from 1 to maxSteps and call the function recursively
 
 def traverse(height, maxSteps, ways):
-    if height < 0:
-        return 0
-    
-    elif height == 0:
-        #ways += 1
-        return 1
-        
-    else:
-        for noSteps in range(1, maxSteps + 1):
-            ways += traverse(height-noSteps, maxSteps, 0)
-            
-    return ways
-        
-print(staircaseTraversal(4,2))
+	if height < 0:
+		return 0
+	
+	elif height == 0:
+		return 1
+		
+	else:
+		for noSteps in range(1, maxSteps + 1):
+			ways += traverse(height-noSteps, maxSteps, 0)
+			
+	return ways
+
+## COMPLEXITY
+# time -> O(h * 2^s) where h is the height of the staircase and s is the maximum steps we can take
+# space: O(n)
