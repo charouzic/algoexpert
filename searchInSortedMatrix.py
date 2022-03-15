32838fb91c79bf48f48a0de5253bcf17e3c2836a
Viktor <vmac@itu.dk>
2022-03-15 17:16:24 +0100

    Adding more optimal solution

--- searchInSortedMatrix.py
+++ searchInSortedMatrix.py
@@ -1,13 +1,35 @@
 def searchInSortedMatrix(matrix, target):
     # Write your code here.
-	# brute force
-	for row in range(len(matrix)):
-		for col in range(len(matrix[0])):
-			if matrix[row][col] == target:
-				return [row, col]
-			
-	return [-1, -1]
+    # brute force
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] == target:
+                return [row, col]
+            
+    return [-1, -1]
 
 ## COMPLEXITY:
 # time -> O(h * w) where h is the height and w is the width of the matrix
 # space -> O(1)
+
+def _searchInSortedMatrix(matrix, target):
+    # Write your code here.
+    
+    # check last column and see if the value is bigger
+         # if bigger => get the previous item
+         # if smaller => go to next line
+    
+    for row in range(len(matrix)):
+        col = len(matrix[0])-1
+        
+        while col >= 0:
+            curVal = matrix[row][col]
+            if curVal == target:
+                return [row,col]
+            elif target > matrix[row][col]:
+                col = -1
+            else:
+                col -= 1
+    
+    return [-1, -1]
+
