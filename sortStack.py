e213e87b71b2cbddb2fafcaffdb2d2a9b124ed81
Viktor <vmac@itu.dk>
2022-03-18 11:22:42 +0100

    Adding working solution for sort stack algorithm

--- sortStack.py
+++ sortStack.py
@@ -5,22 +5,45 @@ def sortStack(stack):
         # .append() ==> appending on top of the stack
         # peek ==> view the last element
         
-    if len(stack) == 1:
+    if len(stack) <= 1:
         return stack
     
-    popedEl = stack.pop()
+    poppedEl = stack.pop()
     
-    sortStack(stack)
+    if poppedEl < stack[-1]:
+        poppedEl2 = stack.pop()
+        stack.append(poppedEl)
+        poppedEl = poppedEl2
+        sortStack(stack)
+    else:
+        sortStack(stack)
     
-    if popedEl > stack[-1]:
-        stack.append(popedEl)
+    if poppedEl > stack[-1]:
+        stack.append(poppedEl)
     else:
         peekedEl = stack.pop()
-        stack.append(popedEl)
+        stack.append(poppedEl)
         stack.append(peekedEl)
         
     return stack
 
-stack = [-5, 2, -2, 4, 3, 1]
+
+def insertAtSortedPosition(stack, value):
+    if len(stack) == 0:
+        stack.append(value)
+        return stack
+    elif stack[-1] > value:
+        poppedVal = stack.pop()
+        insertAtSortedPosition(stack, value)
+        stack.append(poppedVal)
+        return stack
+    else:
+        stack.append(value)
+        
+    return stack
+
+stack = [3, 4, 5, 1, 2]
 
 sortStack(stack)
+
+print(insertAtSortedPosition(stack, 10))
