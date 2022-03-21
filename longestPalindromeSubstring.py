de4a12ab919b4a1fcd70bcf76001c8d0fa6d53a8
Viktor <vmac@itu.dk>
2022-03-21 19:03:30 +0100

    Adding more optimal longest palindrom substring

--- longestPalindromeSubstring.py
+++ longestPalindromeSubstring.py
@@ -36,6 +36,44 @@ def palindrom(string):
         end -= 1
         
     return string
+
+def longestPalindromicSubstring(string):
+    # Write your code here.
+    longestPlaindrom = ""
+    
+    for i in range(len(string)):
+        # index i is the middle
+        
+        substring1 = getPalindrom(string, i - 1, i + 1)
+        
+        substring2 = getPalindrom(string, i - 1, i)
+        
+        if len(substring1) > len(substring2):
+            substring = substring1
+        else:
+            substring = substring2
+        
+        
+        if len(substring) > len(longestPlaindrom):
+            longestPlaindrom = substring
+            
+    return longestPlaindrom
+            
+        
+def getPalindrom(string, left, right):
+    
+    while right < len(string) and left >= 0:
+        if string[left] != string[right]:
+            break
+        else:
+            left -= 1
+            right += 1
+            
+    return string[left + 1 : right]
+    
+## COMPLEXITY
+# time: O(n^2) where n is the number of characters in the string
+# space: O(n)
         
         
 print(longestPalindromicSubstring("abaxyzyxf"))
