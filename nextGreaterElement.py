3c6b8ed1a88d7ee1089ed57fe5bed853cdd7309b
Viktor <vmac@itu.dk>
2022-03-21 18:11:45 +0100

    Adding more efficient next greater element algorithm

--- nextGreaterElement.py
+++ nextGreaterElement.py
@@ -30,6 +30,31 @@ def nextGreaterElement(array):
 # time: O(n^2) where n is the number of elements in the array
 # space: O(n)
 
+def _nextGreaterElement(array):
+    # Write your code here.
+    arrLen = len(array)
+    result =  [-1 for _ in range(arrLen)]
+    
+    stack = []
+    
+    
+    for i in range(2 * arrLen):
+        idx = i % arrLen
+        
+        currentVal = array[idx]
+        
+        while len(stack) > 0 and array[stack[-1]] < currentVal:
+            topOfTheStack = stack.pop()
+            result[topOfTheStack] = currentVal
+        
+        stack.append(idx)
+        
+    return result
+
+## COMPLEXITY
+# time: O(n) where n is the number of elements in the array
+# space: O(n)
+
 array = [2, 5, -3, -4, 6, 7, 2]
 
 nextGreaterElement(array)
