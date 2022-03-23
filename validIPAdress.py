917bbe533ec56bbe86afea7e6158e0505b8ffcb0
Viktor <vmac@itu.dk>
2022-03-23 21:09:03 +0100

    Adding working solution for valid ip address algo

--- validIPAdress.py
+++ validIPAdress.py
@@ -1,19 +1,37 @@
 def validIPAddresses(string):
     # Write your code here.
-    # I need to assign 4 dots across the string
-    validParts = []
-
-    for i in range(len(string)):
-        currNumber = string[i]
-        while currNumber != "00" and int(currNumber) <= 255:
-            validParts.append(currNumber)
-            if i+1 < len(string):
-                currNumber += string[i+1]
-            else:
-                currNumber = "00"
-
-    return validParts
+    ipPart = ""
+    res = []
+    
+    for first in range(1, min(len(string), 4)):
+                       
+        ipAddressParts = ["", "", "", ""]
+            
+        ipAddressParts[0] = string[:first]
         
-string = "1921680"
+        if not isValidPart(ipAddressParts[0]):
+            continue
+            
+        for second in range(first + 1, first + min(len(string) - first, 4)):
+            ipAddressParts[1] = string[first:second]
+        
+            if not isValidPart(ipAddressParts[1]):
+                continue
+            
+            for third in range(second + 1, second + min(len(string) - second, 4)):
+                ipAddressParts[2] = string[second:third]
+                ipAddressParts[3] = string[third:]
 
-print(validIPAddresses(string))
+                if isValidPart(ipAddressParts[2]) and isValidPart(ipAddressParts[3]):
+                    res.append('.'.join(ipAddressParts))
+                    
+    return res
+                               
+                
+
+def isValidPart(number):
+    intNumber = int(number)
+    if intNumber > 255:
+        return False
+    
+    return len(str(intNumber)) == len(str(number))
