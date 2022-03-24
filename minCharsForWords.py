2f1c1a89dd06a7c320a43e7d9bf5007adee38982
Viktor <vmac@itu.dk>
2022-03-24 18:33:23 +0100

    Adding space and time complexity

--- minCharsForWords.py
+++ minCharsForWords.py
@@ -21,6 +21,10 @@ def minimumCharactersForWords(words):
             
     return result
 
+## COMPLEXITY
+# time --> O(w * l) where w is the number of words in the input and l is the number of letters in the longest word
+# space --> O(c) where c is the number of unique letters in the input words
+
 words = ["this", "that", "did", "deed", "them!", "a"]
 
 minimumCharactersForWords(words)
