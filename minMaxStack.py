fa2e1916e2e257752141744c327d4a8cf6ce139e
Viktor <vmac@itu.dk>
2022-03-26 13:12:02 +0100

    Adding space and time complexity to minmaxstack algo

--- minMaxStack.py
+++ minMaxStack.py
@@ -4,17 +4,18 @@ class MinMaxStack:
         self.stack = []
         self.minMaxStack = []
         
+    # time O(1) & space O(1)
     def peek(self):
         # Write your code here.
         return self.stack[-1]
 
-
+    # time O(1) & space O(1)
     def pop(self):
         # Write your code here.
         self.minMaxStack.pop()
         return self.stack.pop()
 
-
+    # time O(1) & space O(1)
     def push(self, number):
         # Write your code here.
         if len(self.minMaxStack) > 0:
@@ -35,6 +36,7 @@ class MinMaxStack:
         self.stack.append(number)
         
 
+    # time O(1) & space O(1)
     def getMin(self):
         # Write your code here.
         if len(self.stack) == 1:
@@ -43,7 +45,7 @@ class MinMaxStack:
         return self.minMaxStack[-1][0]
         
 
-
+    # time O(1) & space O(1)
     def getMax(self):
         # Write your code here.
         if len(self.stack) == 1:
