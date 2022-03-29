321ce1d7f9edf9b530c28d4ad3474bf8a9491c08
Viktor <vmac@itu.dk>
2022-03-29 23:23:23 +0200

    Adding faster solution for array sign algo

--- productSign.py
+++ productSign.py
@@ -9,3 +9,21 @@ def _arraySign(nums):
         return -1
     else:
         return 0
+
+def arraySign(nums):
+    negatives = 0
+    for number in nums:
+        if number == 0:
+            return 0 
+    
+        if number < 0:
+            negatives += 1
+    
+    if negatives%2 != 0:
+        return -1
+    else:
+        return 1
+
+nums = [1,5,0,2,-3]
+
+arraySign(nums)
