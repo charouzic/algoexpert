8784542431afc0a007b8ca6434321e351c44dad2
Viktor <vmac@itu.dk>
2022-04-03 10:56:44 +0200

    Playing with threads in python

---
+++ threadsPlay.py
@@ -0,0 +1,25 @@
+import time
+import threading
+
+
+def printEven(upTo):
+    if upTo > 0:
+        for i in range(upTo):
+            if i % 2 == 0:
+                print(i)
+            time.sleep(0.1)
+
+def printOdd(upTo):
+    if upTo > 0:
+        for i in range(upTo):
+            if i % 2 != 0:
+                print(i)
+            time.sleep(0.1)
+
+even = threading.Thread(target=printEven, args=(20,))
+even.start()
+
+odd  = threading.Thread(target=printOdd, args=(20,))
+odd.start()
+
+
