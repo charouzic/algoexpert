40cc7c8098ff0e5d3084725b46c76a78cdc259a0
Viktor <vmac@itu.dk>
2022-04-03 17:02:01 +0200

    Adding more optimal solution for longest substring without duplication algo

--- longestSubstringWithoutDuplication.py
+++ longestSubstringWithoutDuplication.py
@@ -28,3 +28,26 @@ def longestSubstringWithoutDuplication(string):
 ## COMPLEXITY
 # time: O(n^2) where n is the number of characters in the input string
 # space: O(n)
+
+def _longestSubstringWithoutDuplication(string):
+    # Write your code here.
+    lastSeen = {}
+    startIdx = 0
+    letterRange = [0,1]
+    
+    
+    for i in range(len(string)):
+        currLetter = string[i]
+        if currLetter in lastSeen:
+            startIdx = max(1+lastSeen[currLetter], startIdx)
+        
+        if letterRange[1]-letterRange[0] < i+1 - startIdx:
+            letterRange = [startIdx, i+1]
+
+        lastSeen[currLetter] = i
+        
+    return string[letterRange[0]:letterRange[1]]
+
+## Complexity
+# time: O(n) where n is the number of characters in the string
+# space: O(n)
