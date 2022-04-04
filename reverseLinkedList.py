ddd766b4503304b7c6021fcc11a61b69eb835862
Viktor <vmac@itu.dk>
2022-04-04 20:49:50 +0200

    Initializing algo for reversing linked list

---
+++ reverseLinkedList.py
@@ -0,0 +1,11 @@
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def reverseLinkedList(head):
+    # Write your code here.
+    pass
+
