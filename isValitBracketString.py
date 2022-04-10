c3d0db67d460ea1840a86ab27a8177b5b077662a
Viktor <vmac@itu.dk>
2022-04-10 11:09:04 +0200

    Adding working solution

--- isValitBracketString.py
+++ isValitBracketString.py
@@ -1,2 +1,35 @@
 def isValidBracketString(string):
-    pass
+
+    openingBrackets = "([{"
+    closingBrackets = ")]}"
+    
+    opositeBracket = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    }
+    
+    brackets = []
+    
+    for bracket in string:
+        if bracket in openingBrackets:
+            brackets.append(bracket)
+        elif bracket in closingBrackets: 
+            if len(brackets) == 0:
+                return False
+            
+            stackBracket = brackets.pop()
+            if opositeBracket[stackBracket] != bracket:
+                return False
+                
+    
+    return len(brackets) == 0
+
+
+string = "([])(){}(())()()"
+
+str2 = "(((((({{{{{[[[[[([)])]]]]]}}}}}))))))"
+
+str3 = "(141[])(){waga}((51afaw))()hh()"
+
+print(isValidBracketString(str3))
